72a0d52d663606dc66170ec4f135796d57e3abf7
Jon Young <jey6639@gmail.com>
2023-05-13 19:08:06 -0500

    created javascript page for index.html

---
+++ practice.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+let username = prompt("Welcome to my page, what is your name", "")
+console.log(username)
+
+if (username==="Jon") {
+   alert("That's my name too") 
+    
+} else {
+    alert("Thanks for stopping by "+username)
+
+}
+
+
